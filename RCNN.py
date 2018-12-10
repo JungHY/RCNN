bca6e47023de9a515c4a75809465d79d805496c9
junghayeup <jhyeup@kookmin.ac.kr>
2018-12-10 14:24:38 +0900

    version1

--- RCNN.py
+++ RCNN.py
@@ -10,8 +10,12 @@ import numpy as np
 import skimage.data
 import selectivesearch
 import os
+import random
+from random import shuffle
 
 from mrcnn import model as modellib
+
+RANDOM_SEED = 10
 
 ## Bounding Box regression model
 class BBOX :
@@ -106,7 +110,6 @@ class CLASSIFIER :
         self.model.save(name + '.h5')
 
 
-
 ## RCNN 모델을 만들고 학습하고 테스트하는 클래스
 class R_CNN :
     def __init__(self, warp_shape, output_class) :
@@ -121,6 +124,9 @@ class R_CNN :
         self.BBOX_model = BBOX(self.CNN_layers[4])
         ### build Classifier model & compile model
         self.CLS_model = CLASSIFIER(self.CNN_layers[4], self.output_class)
+        ### build Total model
+        self.TOTAL_model = keras.models.Model(inputs=self.input_image, outputs=[self.BBOX_model.getOutput(), self.CLS_model.getOutput()])
+        self.TOTAL_model.compile(loss='mean_squared_error', optimizer='sgd')
 
     def getIoU(self, scRegion, gtRegion) :
         """
@@ -151,7 +157,7 @@ class R_CNN :
 
 
 
-    def scRegionProposal(self, input_image, gtRegions) :
+    def scRegionProposal(self, input_image, gtRegions, gtClasses) :
         """
         input_image를 입력으로 selective search알고리즘으로 2000개의 candidate region 탐색 후
         ground truth와 비교하여 background인지 object인지 판별
@@ -164,31 +170,38 @@ class R_CNN :
         ### candidate regions 와 background regions
         proposalRegions_idxs = []
         backgroundRegions_idxs = []
+        proposalBBoxes_idxs = []
 
         ### selective search region과 ground truth region의 IoU를 기준으로
         ### 0.7 이상이면 object가 있는 region으로 판단하고
         ### 0.2 미만이면 backgroun region으로 판단
         region_idxs = 0
         for scRegion in scRegions :
+            bbox_idxs = 0
             for gtRegion in gtRegions :
                 if self.getIoU(scRegion, gtRegion) >= 0.7 :
                     proposalRegions_idxs.append(region_idxs)
+                    proposalBBoxes_idxs.append(bbox_idxs)
                 elif self.getIoU(scRegion, gtRegion) < 0.2 :
                     backgroundRegions_idxs.append(region_idxs)
+                bbox_idxs += 1
             region_idxs += 1
 
         proposalRegions = [scRegions[i] for i in proposalRegions_idxs]
         backgrounRegions = [scRegions[i] for i in backgroundRegions_idxs]
+        proposalBBoxes = [gtRegions[i] for i in proposalBBoxes_idxs]
+        proposalRegionsclasses = [gtClasses[i] for i in proposalRegions_idxs]
 
-        return proposalRegions, backgrounRegions
+        return proposalRegions, backgrounRegions, proposalRegionsclasses, proposalBBoxes
 
     def RegionWarp(self, input_image, region) :
 
         ### input_image 원본 이미지
         ### warping 할 영역 (x, y, h, w)
-        ### warping 할 크기 (h, w)
+        ### warping 할 크기 warp_shape
+        x, y, w, h = region
 
-        region_image = input_image[region.y:region.y+h, region.x:region.x+w]
+        region_image = input_image[y:y+h, x:x+w]
         warp_region_image = skimage.transform.warp(region_image, skimage.transform.SimilarityTransform, output_shape=self.warp_shape)
 
         return warp_region_image
@@ -210,19 +223,33 @@ class R_CNN :
         self.model.compile(loss='mean_squared_error', optimizer='sgd')
         return CNN_layers
 
-    def preTrain(self, input_images_dir, input_images_classes) :
+    def preTrain(self, input_images_dir, input_images_classes, gtBBoxes) :
         """
         전체 모델중 CNN의 weight를 미리 학습
+
+        input_images_classes 구조
+        [input_image 개수][gtBBox 개수][class]
+
+        gtBBoxes 구조
+        [input_image 개수][gtBBox 개수][x, y, w, h]
         """
         input_images_names = os.listdir(input_images_dir)
 
-        idx = 0
+        image_idx = 0
         for input_image_name in input_images_names :
             input_image = skimage.io.imread(input_image_name)
-            self.CNNmodel.fit(x=input_image, y=input_images_classes[idx], epochs=2, batch_size=1)
-            idx += 0
+            for i in range(len(input_images_classes[image_idx])) :
+                x, y, w, h = gtBBoxes[image_idx][i]
+                region_class = input_images_classes[image_idx][i]
+                ### region 추출
+                warped_region = self.RegionWarp(input_image, (x,y,w,h))
+                ### region 학습
+                self.CNNmodel.fit(x=warped_region, y=region_class, epochs=1)
+            image_idx += 1
 
-    def RegionShuffle(class_regions, background_regions) :
+        return None
+
+    def RegionShuffle(self, class_regions, background_regions) :
 
         shuffled_regions = class_regions + background_regions
 
@@ -230,8 +257,16 @@ class R_CNN :
 
 
     ## model 훈련
-    def TrainCNN(self, input_images_dir, input_images_classes, gtBBoxes,) :
+    def TrainCNN(self, input_images_dir, input_images_classes, gtBBoxes) :
 
+        """
+        
+        input_images_classes 구조
+        [input_image 개수][gtBBox 개수][class]
+        
+        gtBBoxes 구조
+        [input_image 개수][gtBBox 개수][x, y, w, h]
+        """
 
         ## 1. image directory에서 이미지들 불러오기
         ## 2. (필요시) 픽셀값 0 ~ 1 normalize
@@ -242,33 +277,77 @@ class R_CNN :
         ## 7. positive class와 negative class의 비율 조절
         ## 8. ResNet 모델 학습
 
-        self.preTrain(input_images_dir, input_images_classes)
+
+        ## 
+
+        self.preTrain(input_images_dir, input_images_classes, gtBBoxes)
 
         input_images_names = os.listdir(input_images_dir)
 
+        idx = 0
         for input_image_name in input_images_names :
+            ## image open
             input_image = skimage.io.imread(input_image_name)
-            proposalclassRegions, backgroundclassRegions = self.scRegionProposal(input_image, gtBBoxes)
+            ## proposal region, backgroun 추출
+            objectclassRegions, backgroundclassRegions, objectRegionclasses, objectclassBBoxes  = self.scRegionProposal(input_image, gtBBoxes[idx], gtBBoxes[idx])
+            idx += 1
+            proposal_regions_num = len(objectclassRegions)
+            if len(objectclassRegions)/3 > len(backgroundclassRegions) :
+                background_regions_num = len(backgroundclassRegions)
+            else :
+                background_regions_num = len(objectclassRegions) / 3
 
+            ## object class Region과 background class Region을 비율에 맞게 mix
+            merged_regions = objectclassRegions + backgroundclassRegions[:background_regions_num]
+            merged_classes = objectRegionclasses + [0 for num in range(background_regions_num)]
+            merged_bboxes = objectclassBBoxes + [None for i in range(background_regions_num)]
 
-        feature_vectors = self.model.predict(x=)
+            ## shuffle
+            random.Random(RANDOM_SEED).shuffle(merged_regions)
+            random.Random(RANDOM_SEED).shuffle(merged_classes)
+            random.Random(RANDOM_SEED).shuffle(merged_bboxes)
 
-
+            idx = 0
+            for region in merged_regions :
+                predicted_feature_vectors = self.model.predict(x=region)
+                self.BBOX_model.train(input_feature_vectors=predicted_feature_vectors, ground_truth_bboxs=merged_bboxes[idx], epoch=1, batch_size=1)
+                self.CLS_model.train(input_feature_vectors=predicted_feature_vectors, ground_truth_classes=merged_classes[idx], epoch=1, batch_size=1)
+                idx += 1  
 
         return None
 
     ## image test
     def TestRCNN(self, input_image) :
 
+        predicted_classes = []
+        predicted_bboxes = []
 
-        return None
+        if type(input_image) == type('') :
+            input_image = skimage.io.imread(input_image)
+
+
+        _, sc_regions = selectivesearch.selective_search(input_image)
+        proposal_regions = []
+        for region in sc_regions :
+            proposal_regions.append([region['rect'][0], region['rect'][1], region['rect'][2]- region['rect'][0], region['rect'][3] - region['rect'][1] ])
+
+        warped_region = []
+        for region in proposal_regions :
+            warped_region.append(self.RegionWarp(input_image, region))
+
+        for region in warped_region :
+            bbox, class_ = self.TOTAL_model.predict(region)
+            predicted_bboxes.append( bbox )
+            predicted_classes.append( class_ )
+
+        predict_results = []
+        for idx in range(len(predicted_bboxes)) :
+            predict_results.append([predicted_bboxes[idx][0], predicted_bboxes[idx][1], predicted_bboxes[idx][2], predicted_bboxes[idx][3], predicted_classes[idx]])
+
+        return predict_results
 
     ## model 저장
     def SaveModel(self) :
-        return None
-
-    def LoadModel(self) :
-        return None
-
+        self.TOTAL_model.save('total_model.h5')
 
     ### mac book git test
